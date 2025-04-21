13d3397ce7d811ef3e775862b90b62a90f65506b
Jherson Castro <imbucket@outlook.com>
2025-04-21 15:24:48 -0500

    Change C# version MSTEST 8.0 to 9.0

---
+++ TestBench/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace TestBench
+{
+    [TestClass]
+    public sealed class UserServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}

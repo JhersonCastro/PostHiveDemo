f480475cae5c4ca3a49f69a18a924cd6d40bf6e8
JhersonCastro <jhersoncastro@unicauca.edu.co>
2025-04-21 22:31:54 -0500

    Change Style for post and add a funny 404 page

---
+++ PostHive/Middleware/NotFoundMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace PostHive.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class NotFoundMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public NotFoundMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await _next(httpContext);
+            if (httpContext.Response.StatusCode == 404)
+            {
+                httpContext.Response.Redirect("/404");
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class NotFoundMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseNotFoundMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<NotFoundMiddleware>();
+        }
+    }
+}

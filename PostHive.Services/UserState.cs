0c36a944cf8aff44c5e885f7d415424f9909e8c6
JhersonCastro <jhersoncastro@unicauca.edu.co>
2025-06-08 13:21:58 -0500

    Update UserState.cs

--- PostHive.Services/UserState.cs
+++ PostHive.Services/UserState.cs
@@ -2,8 +2,15 @@
 
 namespace PostHive.Services
 {
+    /// <summary>
+    /// Represents the state of the currently logged-in user.
+    /// </summary>
     public class UserState
     {
+        /// <summary>
+        /// Gets or sets the current user.
+        /// This property holds the information of the user currently logged into the application.
+        /// </summary>
         public User? CurrentUser { get; set; }
     }
 }

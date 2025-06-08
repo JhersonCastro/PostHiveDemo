915325c82c8077265d5939428188573c29e76d48
JhersonCastro <142602493+JhersonCastro@users.noreply.github.com>
2025-06-08 18:16:15 -0500

    Update Const.cs
    Add BlobContainerUrl to use blob containers insted of server local (it's more faster)

--- PostHive/Const.cs
+++ PostHive/Const.cs
@@ -8,10 +8,12 @@
         public const string url = "http://posthive.tryasp.net";
         //public const string url = "http://localhost:5016";
         //public const string url = "https://posthive.me";
+        public const string BlobContainerUrl = "https://posthiveblob.blob.core.windows.net/posthivecontainer/";
 
         public const int MaxFileSize = 5242880;
         public const byte MaxFileCount = 5;
         public const long MaxFileSizePost = 512*Mb;
         public const int Mb = 1024 * 1024;
+        
     }
 }

abef6d7bbcaeeaf3712ea23713bd64e4a4af99ab
JhersonCastro <jhersoncastro@unicauca.edu.co>
2025-06-08 13:21:52 -0500

    Update ThemeState.cs

--- PostHive.Services/ThemeState.cs
+++ PostHive.Services/ThemeState.cs
@@ -2,9 +2,14 @@
 {
     /// <summary>
     /// Theme state class
+    /// This class is used to manage the theme state of the application.
+    /// It contains a property to determine if the dark theme is enabled.
     /// </summary>
     public class ThemeState
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether the dark theme is enabled.
+        /// </summary>
         public bool IsDark { get; set; }
     }
 }
